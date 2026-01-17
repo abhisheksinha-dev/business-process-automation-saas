830cc637072cb6d03c0ba6603ab7172ea8b0ac64
Abhishek Sinha <techabhi2000@gmail.com>
2026-01-17 19:30:07 +0530

    extended SwaggerConfig

--- src/main/java/com/abhishek/bpa/config/SwaggerConfig.java
+++ src/main/java/com/abhishek/bpa/config/SwaggerConfig.java
@@ -1,13 +1,18 @@
 package com.abhishek.bpa.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String BEARER_SCHEME = "bearerAuth";
 
     @Bean
     public OpenAPI openAPI(){
@@ -21,6 +26,17 @@ public class SwaggerConfig {
                                 .email("techabhi2000@gmail.com")
                         )
 
+                )
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                BEARER_SCHEME,
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
                 );
     }
 }
