3692fd58046bdbd3d0c38a5b35c345f9424ab77a
Abhishek Sinha <techabhi2000@gmail.com>
2026-01-17 19:37:53 +0530

    Added constants for endpoint and message.

---
+++ src/main/java/com/abhishek/bpa/constant/EndPointConstant.java
@@ -0,0 +1,14 @@
+package com.abhishek.bpa.constant;
+
+public class EndPointConstant {
+
+    public static final String BASE_ENDPOINT = "/api/v1";
+
+    public static final String AUTH = BASE_ENDPOINT + "/auth";
+    public static final String SIGN_UP = "/sign-up";
+    public static final String LOGIN = "/login";
+
+    public static final String USER = BASE_ENDPOINT + "/users";
+    public static final String GET_ALL = "/get-all";
+
+}
---
+++ src/main/java/com/abhishek/bpa/constant/MessageConstant.java
@@ -0,0 +1,8 @@
+package com.abhishek.bpa.constant;
+
+public class MessageConstant {
+
+    public static final String INVALID_CREDENTIALS = "Invalid Credentials";
+    public static final String INACTIVE_USER = "User is inactive";
+    public static final String EMAIL_ALREADY_REGISTERED = "Email already registered";
+}
