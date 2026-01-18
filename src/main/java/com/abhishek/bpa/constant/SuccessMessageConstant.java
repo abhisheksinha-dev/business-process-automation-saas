d6a0b59d4c09adb5b58baa832df25ee2c49f8a39
Abhishek Sinha <techabhi2000@gmail.com>
2026-01-18 14:25:03 +0530

    Added constants.

---
+++ src/main/java/com/abhishek/bpa/constant/SuccessMessageConstant.java
@@ -0,0 +1,10 @@
+package com.abhishek.bpa.constant;
+
+public class SuccessMessageConstant {
+
+    public static final String SIGNUP = "Sign up successful";
+    public static final String LOGIN = "Login Successful";
+    public static final String WORKSPACE_NOT_FOUND = "No workspaces found";
+    public static final String WORKSPACES_FOUND = "Available workspaces";
+    public static final String USERS_FETCHED = "Users fetched successfully";
+}
