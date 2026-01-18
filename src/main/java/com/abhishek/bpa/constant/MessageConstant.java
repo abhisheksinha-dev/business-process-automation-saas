8059787239392da639bb86c11c743b933a7b76c1
Abhishek Sinha <techabhi2000@gmail.com>
2026-01-18 08:51:38 +0530

    Added EndPoint and MessageConstant

--- src/main/java/com/abhishek/bpa/constant/MessageConstant.java
+++ src/main/java/com/abhishek/bpa/constant/MessageConstant.java
@@ -4,5 +4,8 @@ public class MessageConstant {
 
     public static final String INVALID_CREDENTIALS = "Invalid Credentials";
     public static final String INACTIVE_USER = "User is inactive";
-    public static final String EMAIL_ALREADY_REGISTERED = "Email already registered";
+    public static final String SIGNUP_FAILED_DUPLICATE_DATA =
+            "An account with the same email, phone, or organization already exists";
+    public static final String INVALID_WORKSPACE = "Invalid workspace";
+    public static final String WORKSPACE_NOT_FOUND = "Workspace not found";
 }
